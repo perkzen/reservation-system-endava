8847a79e29e3eeee2a804b531f5da3454de02cb8
perkzen <perko.domen@gmail.com>
2022-05-10 21:05:35 +0200

    feat: backend configuration

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "The API is up and running.!"', () => {
+      expect(appController.getPing()).toBe('The API is up and running.');
     });
   });
 });
