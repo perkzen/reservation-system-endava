523c4afe77d364f4c603903dcc492820c6b6cc5b
perkzen <perko.domen@gmail.com>
2022-05-10 21:41:53 +0200

    feat: add .env

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -1,8 +1,10 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_URL = "http://localhost:8080/api/v1";
+const BASE_URL = process.env.REACT_APP_BE_BASE_URL;
+const API_VERSION = process.env.REACT_APP_API_VERSION || 'v1';
+
 const instance = axios.create({
-  baseURL: BASE_URL
+  baseURL: BASE_URL + API_VERSION,
 });
 
 export default instance;
