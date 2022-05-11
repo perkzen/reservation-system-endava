adfebb0e3927d208e953b0bc6cfdcaf0d29d22e4
perkzen <perko.domen@gmail.com>
2022-05-11 19:01:15 +0200

    fix: add button

--- frontend/src/components/ui/Button/Button.tsx
+++ frontend/src/components/ui/Button/Button.tsx
@@ -1,12 +1,23 @@
 import React, { FC, ReactNode } from 'react';
 import classes from './Button.module.scss';
+import { classNames } from '../../../utils/classNames';
 
 interface ButtonProps {
   children: ReactNode;
+  onClick?: () => void;
+  className?: string;
 }
 
-const Button: FC<ButtonProps> = ({ children }) => {
-  return <button className={classes.Container}>{children}</button>;
+const Button: FC<ButtonProps> = ({ children, onClick, className = '' }) => {
+  return (
+    <button
+      className={classNames(className, classes.Container)}
+      onClick={onClick}
+      type={'submit'}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
