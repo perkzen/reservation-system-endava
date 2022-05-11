c3d1f6fec324b06b1affadc8ea1a71cea54f496a
perkzen <perko.domen@gmail.com>
2022-05-11 18:24:05 +0200

    feat: add auth slice

--- frontend/src/store/app/store.ts
+++ frontend/src/store/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 import globalReducer from '../features/globalSlice';
+import authReducer from '../features/authSlice';
 
 const sagaMiddleware = createSagaMiddleware();
 export const store = configureStore({
   reducer: {
     global: globalReducer,
+    auth: authReducer,
   },
   middleware: (defaultMiddleware) => [
     ...defaultMiddleware({ thunk: false, serializableCheck: false }),
---
+++ frontend/src/store/features/authSlice.ts
@@ -0,0 +1,26 @@
+import { User } from '../models/Auth';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export interface AuthState {
+  user: User | null;
+}
+
+const initialState: AuthState = {
+  user: null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    login: (state, action: PayloadAction<User>) => {
+      state.user = action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
