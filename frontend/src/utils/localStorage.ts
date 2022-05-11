951693cb0f443747f775a5e48266c302ee177349
perkzen <perko.domen@gmail.com>
2022-05-11 16:19:11 +0200

    feat: add utils and user storage

---
+++ frontend/src/utils/localStorage.ts
@@ -0,0 +1,20 @@
+import { User } from "../store/models/Auth";
+
+const user_prefix = 'user';
+
+export const userStorage = {
+  getUser: (): User | undefined => {
+    if (typeof window !== 'undefined')
+      return JSON.parse(
+        window.localStorage.getItem(`${user_prefix}`) as string
+      ) as User;
+  },
+  setUser: (user: User): void => {
+    if (typeof window !== 'undefined')
+      window.localStorage.setItem(`${user_prefix}`, JSON.stringify(user));
+  },
+  clearUser: (): void => {
+    if (typeof window !== 'undefined')
+      window.localStorage.removeItem(`${user_prefix}`);
+  },
+};
