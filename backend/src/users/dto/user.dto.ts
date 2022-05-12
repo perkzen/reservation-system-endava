d65214daaad8634b9de1ce3d886a5597efb34180
perkzen <perko.domen@gmail.com>
2022-05-12 23:17:16 +0200

    feat: add create new user service

---
+++ backend/src/users/dto/user.dto.ts
@@ -0,0 +1,7 @@
+export class UserDto {
+  id: string;
+  firstname: string;
+  surname: string;
+  location: string;
+  admin: boolean;
+}
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
+import { UserDto } from './dto/user.dto';
+import { User } from './schemas/user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly userRepository: UsersRepository) {}
+
+  async createUser(user: UserDto): Promise<User> {
+    return this.userRepository.create(user);
+  }
+}
