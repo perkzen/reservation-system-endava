f1e0e353d3ee7063da4065900d58325cde02eb82
perkzen <perko.domen@gmail.com>
2022-05-12 16:18:08 +0200

    feat: add side menu

--- frontend/src/components/ui/LayoutProvider/LayoutProvider.tsx
+++ frontend/src/components/ui/LayoutProvider/LayoutProvider.tsx
@@ -10,7 +10,15 @@ const LayoutProvider: FC<LayoutProps> = ({ children }) => {
   return (
     <div className={classes.Container}>
       <SideMenu />
-      {/*<main>{children}</main>*/}
+      <main className="flex-1">
+        <div className="py-6">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+            {/* Replace with your content */}
+            <div className="py-4">{children}</div>
+            {/* /End replace */}
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
