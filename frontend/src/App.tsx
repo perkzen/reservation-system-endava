7925a1fc7f3bff580068f01589089495bb5f3eb9
perkzen <perko.domen@gmail.com>
2022-05-12 22:01:38 +0200

    feat: add LanguageProvider.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,18 @@
 import React from 'react';
 import { Router } from './components/pages/Router/Router';
 import ModalProvider from './components/ui/ModalProvider/ModalProvider';
-import { initReactI18next } from 'react-i18next';
-import i18n from 'i18next';
-import translationsEn from './i18n/en/translations.json';
 import AuthProvider from './components/ui/AuthProvider/AuthProvider';
-
-i18n.use(initReactI18next).init({
-  resources: {
-    en: { translation: translationsEn },
-  },
-  lng: 'en',
-  fallbackLng: 'en',
-  interpolation: { escapeValue: false },
-});
+import LanguageProvider from './components/ui/LanguageProvider/LanguageProvider';
 
 function App() {
   return (
-    <AuthProvider>
-      <ModalProvider>
-        <Router />
-      </ModalProvider>
-    </AuthProvider>
+    <LanguageProvider>
+      <AuthProvider>
+        <ModalProvider>
+          <Router />
+        </ModalProvider>
+      </AuthProvider>
+    </LanguageProvider>
   );
 }
 
