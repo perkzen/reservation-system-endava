a6834fecf101a92614aa571fe346123427640767
perkzen <perko.domen@gmail.com>
2022-05-13 17:28:10 +0200

    feat: add swagger to ping method

--- backend/src/app/app.service.ts
+++ backend/src/app/app.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { ApiOkResponse } from '@nestjs/swagger';
 
 @Injectable()
 export class AppService {
+  @ApiOkResponse({ description: 'Api is up and running' })
   getPing(): string {
     return 'The API is up and running.';
   }
