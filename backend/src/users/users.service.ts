926d7678ef05e533b18d3cddaa3753c96a57b788
perkzen <perko.domen@gmail.com>
2022-05-13 17:13:13 +0200

    feat: add create, get, update user details

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,13 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
-import { UserDto } from './dto/user.dto';
+import { UserDetailsDto } from './dto/userDetails.dto';
 import { User } from './schemas/user.schema';
+import { Errors } from '../utils/constants/errors';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly userRepository: UsersRepository) {}
 
-  async createUser(user: UserDto): Promise<User> {
-    return this.userRepository.create(user);
+  async createUserDetails(user: UserDetailsDto) {
+    const found = this.userRepository.findOne({ id: user.id });
+
+    if (found) {
+      throw new HttpException(
+        Errors.USER_ALREADY_EXISTS,
+        HttpStatus.PRECONDITION_FAILED,
+      );
+    }
+
+    await this.userRepository.create(user);
+  }
+
+  async getUserDetails(id: string): Promise<User> {
+    const found = this.userRepository.findOne({ id: id });
+
+    if (!found) {
+      throw new HttpException(Errors.USER_NOT_FOUND, HttpStatus.NOT_FOUND);
+    }
+
+    return await this.userRepository.findOne({ id: id });
+  }
+
+  async updateUserDetails(user: UserDetailsDto) {
+    const found = this.userRepository.findOne({ id: user.id });
+
+    if (!found) {
+      throw new HttpException(Errors.USER_NOT_FOUND, HttpStatus.NOT_FOUND);
+    }
+
+    await this.userRepository.findOneAndUpdate({ id: user.id }, user);
   }
 }
