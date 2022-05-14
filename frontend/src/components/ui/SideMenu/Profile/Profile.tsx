9af1da9e76551b0c8215d84bc2e68c60edb257e2
perkzen <perko.domen@gmail.com>
2022-05-14 15:08:25 +0200

    feat: add authentication

--- frontend/src/components/ui/SideMenu/Profile/Profile.tsx
+++ frontend/src/components/ui/SideMenu/Profile/Profile.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import classes from './Profile.module.scss';
 import { Link } from 'react-router-dom';
+import { useAppSelector } from '../../../../store/app/hooks';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../../../firebase-config';
 
 const Profile = () => {
+  const { user } = useAppSelector((state) => state.user);
+
+  const logout = async () => {
+    await signOut(auth);
+  };
+
   return (
     <div className={classes.Container}>
       <Link to={''} className={classes.Link}>
         <div className={classes.Flex}>
-          <img src={''} alt="Profile" />
+          <img src={user?.photoURL} alt="Profile" />
           <div>
-            <p>{'Domen Perko'}</p>
-            <p>View profile</p>
+            <p>{user?.email}</p>
+            <p onClick={logout}>Logout</p>
           </div>
         </div>
       </Link>
