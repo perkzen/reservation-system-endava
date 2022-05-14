d9c4cde6a1a8892019e33c4df157f5c7af9dfb94
perkzen <perko.domen@gmail.com>
2022-05-14 19:29:07 +0200

    feat: add page not found

--- frontend/src/components/pages/Router/UnauthenticatedRouter.tsx
+++ frontend/src/components/pages/Router/UnauthenticatedRouter.tsx
@@ -3,13 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import { routes } from '../../../routes';
 import Login from '../Login/Login';
 import Register from '../Register/Register';
+import PageNotFound from '../PageNotFound/PageNotFound';
 
 export const UnauthenticatedRouter: FC = () => {
   return (
     <Routes>
       <Route path={routes.LOGIN} element={<Login />} />
       <Route path={routes.REGISTER} element={<Register />} />
-      {/*<Route path={routes.PAGE_NOT_FOUND} element={<PageNotFound />} />*/}
+      <Route path={routes.PAGE_NOT_FOUND} element={<PageNotFound />} />
     </Routes>
   );
 };
