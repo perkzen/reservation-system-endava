33c903d910ba5bbaa23a6b2a17af364f3a1f320f
jernej10 <jernej.ofic@gmail.com>
2022-05-14 21:19:10 +0200

    Feat: add office

--- frontend/src/components/pages/Home/Home.tsx
+++ frontend/src/components/pages/Home/Home.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from 'react';
+import OfficeRoom from '../../ui/Office/OfficeRoom';
 
 const Home: FC = () => {
-  return <div>Home page</div>;
+  return (
+    <div>
+      <OfficeRoom row={3} column={5}></OfficeRoom>
+    </div>
+  );
 };
 
 export default Home;
