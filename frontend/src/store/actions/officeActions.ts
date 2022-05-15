c979961ddb952e7d6ebad2a20298052d242517ed
perkzen <perko.domen@gmail.com>
2022-05-15 09:05:08 +0200

    feat: add office actions

--- frontend/src/store/actions/officeActions.ts
+++ frontend/src/store/actions/officeActions.ts
@@ -1,4 +1,10 @@
 import { Office } from '../models/Office';
 import { createAction } from '@reduxjs/toolkit';
 
-export const createOffice = createAction<Office>('');
+const OFFICE_SLICE = 'office';
+
+export const saveOffice = createAction<Office>(`${OFFICE_SLICE}/saveOffice`);
+
+export const saveOfficeSuccess = createAction(
+  `${OFFICE_SLICE}/saveOfficeSuccess`
+);
