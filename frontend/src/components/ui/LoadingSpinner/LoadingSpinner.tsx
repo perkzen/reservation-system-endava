6b729d4cd6cf28cc19be49cf208526e3283297f0
perkzen <perko.domen@gmail.com>
2022-05-18 17:34:39 +0200

    fix: rename

--- frontend/src/components/ui/LoadingSpinner/LoadingSpinner.tsx
+++ frontend/src/components/ui/LoadingSpinner/LoadingSpinner.tsx
@@ -5,7 +5,7 @@ import loading from '../../../assets/loading.gif';
 const LoadingSpinner = () => {
   return (
     <div className={classes.Container}>
-      <img src={loading} alt="Company Logo" />
+      <img src={loading} alt="Loading spinner" />
     </div>
   );
 };
