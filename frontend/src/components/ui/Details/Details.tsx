8183ff2e625d7b4732a17b6826f65421d4585d25
perkzen <perko.domen@gmail.com>
2022-05-19 11:01:43 +0200

    fix: import

--- frontend/src/components/ui/Details/Details.tsx
+++ frontend/src/components/ui/Details/Details.tsx
@@ -1,11 +1,11 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { InformationCircleIcon } from '@heroicons/react/outline';
 import classes from './Details.module.scss';
 import { useTranslation } from 'react-i18next';
 
 const Details: FC = () => {
   const { t } = useTranslation();
-  const [showLegend, setShowLegend] = React.useState<boolean>(false);
+  const [showLegend, setShowLegend] = useState<boolean>(false);
 
   return (
     <div className={classes.Container}>
