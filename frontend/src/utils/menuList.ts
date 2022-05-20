f10645bc65bbc0b6ac836e2d7a14e3ec0aa06e04
gregorsulcer <gregor.sulcer@student.um.si>
2022-05-20 14:11:03 +0200

    fix: clean up

--- frontend/src/utils/menuList.ts
+++ frontend/src/utils/menuList.ts
@@ -9,30 +9,29 @@ export const generateMenu = (offices: Office[]) => {
   for (let i = 0; i < offices.length; i++) {
     locations.push(offices[i].location);
   }
+
   // @ts-ignore
-  let uniqueLocations: string = [...new Set(locations)];
+  let uniqueLocations: string[] = [...new Set(locations)];
   //menu parent elements aka. locations
   for (let i = 1; i <= uniqueLocations.length; i++) {
-    const json_object = {
+    data.push({
       id: i,
       parent: 0,
       droppable: true,
       text: uniqueLocations[i - 1],
-    };
-    data.push(json_object);
+    });
   }
 
   //menu child elements
   for (let i = 0; i < offices.length; i++) {
     for (let j = 0; j < uniqueLocations.length; j++) {
       if (offices[i].location === uniqueLocations[j]) {
-        const json_object = {
+        data.push({
           id: offices[i]._id, //uniqueLocations.length + (i + 1) if we want id sequence, we need uuid's ?
           parent: j,
           droppable: false,
           text: offices[i].name,
-        };
-        data.push(json_object);
+        });
       }
     }
   }
