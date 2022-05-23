3d0473bc26667f38b51ce356a28a2f036f5ace10
perkzen <perko.domen@gmail.com>
2022-05-24 00:39:20 +0200

    feat: update office component

--- frontend/src/components/ui/Office/Office.tsx
+++ frontend/src/components/ui/Office/Office.tsx
@@ -8,18 +8,29 @@ import { v4 } from 'uuid';
 import { useAppDispatch } from '../../../store/app/hooks';
 import { addModal } from '../../../store/features/globalSlice';
 import { ModalType } from '../../../store/models/Modal';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 
 interface OfficeProps {
-  office: OfficeModel;
-  currentDate: Date;
-  from: number;
-  to: number;
+  office?: OfficeModel;
+  currentDate?: Date;
+  from?: number;
+  to?: number;
+  loading?: boolean;
+  emptyText?: string;
 }
 
-const Office: FC<OfficeProps> = ({ office, currentDate, from, to }) => {
+const Office: FC<OfficeProps> = ({
+  office,
+  currentDate,
+  from,
+  to,
+  loading,
+  emptyText,
+}) => {
   const dispatch = useAppDispatch();
 
   const handleClick = (workspaceId: string) => {
+    if (!office) return;
     dispatch(
       addModal({
         type: ModalType.RESERVATION,
@@ -36,19 +47,38 @@ const Office: FC<OfficeProps> = ({ office, currentDate, from, to }) => {
   };
 
   return (
-    <div className={classes.Container} style={grid(office.cols, office.rows)}>
-      {gridToArray(office.cols, office.rows).map((pos) =>
-        positionWorkspace(pos, office.workspaces) ? (
-          <Workspace
-            key={v4()}
-            workspace={findWorkspace(pos, office.workspaces)}
-            onClick={handleClick}
-          />
-        ) : (
-          <div key={v4()} />
-        )
+    <>
+      {loading ? (
+        <div className={classes.EmptyContainer}>
+          <LoadingSpinner />
+        </div>
+      ) : (
+        <>
+          {office ? (
+            <div
+              className={classes.Container}
+              style={grid(office.cols, office.rows)}
+            >
+              {gridToArray(office.cols, office.rows).map((pos) =>
+                positionWorkspace(pos, office.workspaces) ? (
+                  <Workspace
+                    key={v4()}
+                    workspace={findWorkspace(pos, office.workspaces)}
+                    onClick={handleClick}
+                  />
+                ) : (
+                  <div key={v4()} />
+                )
+              )}
+            </div>
+          ) : (
+            <div className={classes.EmptyContainer}>
+              <h1>{emptyText}</h1>
+            </div>
+          )}
+        </>
       )}
-    </div>
+    </>
   );
 };
 
