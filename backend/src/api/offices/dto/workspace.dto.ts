fa586c44f39b155e776073fd3b852f4ca29a6a00
perkzen <perko.domen@gmail.com>
2022-05-25 23:17:08 +0200

    fix: update seat when reservation successfull

--- backend/src/api/offices/dto/workspace.dto.ts
+++ backend/src/api/offices/dto/workspace.dto.ts
@@ -1,20 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsNumber, IsOptional, IsString } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class WorkspaceDto {
-  // @ApiProperty({ required: true })
-  // @IsString()
-  // @IsOptional()
-  // id: string;
+  @ApiProperty({ required: true })
+  @IsString()
+  @IsOptional()
+  userId: string;
 
   @ApiProperty({ required: true })
   @IsNumber()
   position: number;
-
-  // @ApiProperty({ required: true })
-  // @IsBoolean()
-  // @IsOptional()
-  // reserved: boolean;
 
   @ApiProperty({ required: true })
   @IsString()
