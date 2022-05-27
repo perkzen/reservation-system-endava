a034324fbd877565bd3be0b9ae9eb3364ff5fff6
perkzen <perko.domen@gmail.com>
2022-05-27 13:43:21 +0200

    fix: add shadow

--- frontend/src/components/ui/Toggle/Toggle.tsx
+++ frontend/src/components/ui/Toggle/Toggle.tsx
@@ -24,7 +24,7 @@ const Toggle: FC<ToggleProps> = ({ handleChangeToggle, checked, label }) => {
           "peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] " +
           'after:absolute after:top-[2px] after:left-[2px] ' +
           'after:bg-white after:border-gray-300 after:border after:rounded-full ' +
-          'after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary'
+          'after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary shadow-md'
         }
       />
       <span>{label}</span>
